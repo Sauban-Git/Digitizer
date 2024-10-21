e1e37c881d0b2d4823c8bef87c38e394b7ee0e79
sauban <git.github@noreply.com>
2024-10-21 13:07:36 +0530

    Added Model function

--- app/src/main/java/com/sauban/digitizer/CoreModel.kt
+++ app/src/main/java/com/sauban/digitizer/CoreModel.kt
@@ -12,7 +12,17 @@ class CoreModel {
         return  expZ.map { it / sumExpZ }.toDoubleArray()
     }
 
-    fun forward ( W1: Array<DoubleArray>, b1: DoubleArray,, W2: Array<DoubleArray>, b2: DoubleArray, X: DoubleArray): Triple<Triple<DoubleArray, DoubleArray>, DoubleArray, DoubleArray> {
+    fun forwardProp(W1: Array<DoubleArray>, b1: DoubleArray, W2: Array<DoubleArray>, b2: DoubleArray, X: DoubleArray): Triple<Triple<DoubleArray, DoubleArray>, DoubleArray, DoubleArray> {
+        val Z1 = W1.map { row -> row.mapIndexed { index, value -> value * X[index] }.sum() + b1[W1.indexOf(row)] }.toDoubleArray()
+        val A1 = relu(Z1)
         
+        val Z2 = W2.map { row -> row.mapIndexed { index, value -> value * A1[index] }.sum() + b2[W2.indexOf(row)] }.toDoubleArray()
+        val A2 = softmax(Z2)
+        
+        return Triple(Triple(Z1, A1, Z2), A2)
+    }
+    
+    fun getPredictions(A2: DoubleArray): Int {
+        return A2.indices.maxByOrNull { A2[it] } ?: -1
     }
 }
